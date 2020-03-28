01ef2c1052e0f968d05b455d207bf2f7dcc9652c
songchenwen <me@songchenwen.com>
2020-03-29 00:04:56 +0800

    Change Release Name

--- rebase_on_lean.sh
+++ rebase_on_lean.sh
@@ -5,9 +5,12 @@ cd friendlywrt
 git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
 git remote add upstream https://github.com/coolsnowwolf/lede && git fetch upstream
 git rebase adc1a9a3676b8d7be1b48b5aed185a94d8e42728^ --onto upstream/master -X theirs
+echo ""
+git status
+echo ""
 git revert --no-edit f092ca098e80c667b10cdd1dba328506a2673c1d
 git checkout upstream/master -- feeds.conf.default
-
+echo ""
 git status
 echo ""
 echo "feeds.conf.default"
